2a65459d5e429ebee7682926a4ef7747d5921994
Adrijchakraborty <adrijchakraborty8@gmail.com>
2024-11-18 22:29:04 +0530

    Friend sugession logic complete

--- Backend/controller/friend.controller.js
+++ Backend/controller/friend.controller.js
@@ -1,4 +1,5 @@
 import Auth from "../models/auth.model.js";
+import Friend from "../models/friend.model.js";
 import { createError } from "../utils/createError.js";
 
 export const friendList = async (req, res, next) => {
@@ -34,13 +35,19 @@ export const findUsersWithCommonTopics = async (req, res, next) => {
         const user = await Auth.findById(req.user.id);
         if (!user) return next(createError(404, "User not found"));
 
+        const user_friendlist = await Friend.findOne({Ref : user._id});
+
+
         const limit = parseInt(req.query.limit) || 5;
         const skip = parseInt(req.query.skip) || 0;
 
         const similarUsers = await Auth.find(
             {
                 topics: { $in: user.topics },
-                _id : { $ne : user._id}
+                _id : { 
+                    $ne : user._id,
+                    $nin : user_friendlist.following
+                }
             }).limit(limit).skip(skip);
 
         res.status(200).json(similarUsers);
@@ -48,3 +55,12 @@ export const findUsersWithCommonTopics = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getFriendList = async(req, res,next) => {
+    try {
+        const friendList = await Friend.findOne({Ref : req.user.id})
+        res.status(200).json(friendList);
+    } catch (error) {
+        next(error);
+    }
+}
--- Backend/router/friend.router.js
+++ Backend/router/friend.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import {verifyToken} from "../utils/verifyToken.js"
-import { findUsersWithCommonTopics, friendList } from '../controller/friend.controller.js';
+import { findUsersWithCommonTopics, friendList, getFriendList } from '../controller/friend.controller.js';
 
 const router = express.Router();
 
-router.post('/friends', verifyToken , friendList);
+router.post('/add-friends', verifyToken , friendList);
+router.get('/get-friendlist',verifyToken, getFriendList);
 router.get('/similar-users', verifyToken, findUsersWithCommonTopics);
 
 export default router;
